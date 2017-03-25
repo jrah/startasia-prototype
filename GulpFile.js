1c1e2c02b5290b97aa34ef52ace5f371138de5ff
jrah <jjames.home@gmail.com>
2017-03-25 22:48:37 +0000

    Add build task

--- GulpFile.js
+++ GulpFile.js
@@ -1,7 +1,11 @@
 var gulp        = require('gulp');
 var browserSync = require('browser-sync').create();
+// sass
 var sass        = require('gulp-sass');
-
+// js
+var pump = require('pump');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
 // Static Server + watching scss/html files
 gulp.task('serve', ['sass'], function() {
 
@@ -28,4 +32,31 @@ gulp.task('sass', function() {
         .pipe(browserSync.stream());
 });
 
+gulp.task('copy-build', () => gulp
+  .src('*.html')
+  .pipe(gulp.dest('dist')));
+
+gulp.task('sass-build', function() {
+     gulp.src("src/scss/*.scss")
+         .pipe(sass({
+           includePaths: [
+            './bower_components/mathsass/dist/',
+            './bower_components/madsauce/',
+            './bower_components/'
+         ]
+       }))
+        .pipe(gulp.dest("dist/css"))
+});
+
+gulp.task('js-build', function () {
+  // pump helps locate errors better than `pipe`
+  pump([
+    gulp.src('js/*.js'),
+    uglify(),
+    concat('app.js'),
+    gulp.dest('dist/js')
+  ])
+});
+
 gulp.task('default', ['serve']);
+gulp.task('build', ['js-build', 'copy-build', 'sass-build']);
