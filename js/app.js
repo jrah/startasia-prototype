e421cdee0ba017516963186574ec80667c818794
jrah <jjames.home@gmail.com>
2017-04-23 04:29:47 +0100

    Fix image load with masonry

--- js/app.js
+++ js/app.js
@@ -1,8 +1,13 @@
-var elem = document.querySelector('.card');
-var msnry = new Masonry( elem, {
-  // options
-  itemSelector: '.card-item',
-  columnWidth: '.card-column',
-  gutter: '.card-gutter',
-  // percentPosition: true
+imagesLoaded( document.querySelector('.card'), function( instance ) {
+  console.log('all images are loaded');
+  var elem = document.querySelector('.card');
+  console.log('masonry loaded');
+  var msnry = new Masonry( elem, {
+    // options
+    itemSelector: '.card-item',
+    columnWidth: '.card-column',
+    gutter: '.card-gutter',
+    // percentPosition: true
+  });
+
 });
